4dd80036e9f5babbefb27af3ca5cb68d1c03dfd6
Aleksandr Lobov <a.lobov@basis.center>
2023-04-14 21:54:46 +0300

    change name

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Александр Лобов'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Резюме'
 
 export default function Layout({ children, home }) {
   return (
